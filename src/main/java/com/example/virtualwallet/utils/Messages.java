7286609d4f8265a922845c94b30189700b3abb03
sbalieva <147823207+sbalieva@users.noreply.github.com>
2024-02-27 13:16:53 +0200

    Correction in messages.

--- src/main/java/com/example/virtualwallet/utils/Messages.java
+++ src/main/java/com/example/virtualwallet/utils/Messages.java
@@ -1,22 +1,14 @@
 package com.example.virtualwallet.utils;
 
 public class Messages {
-    public static final String UNAUTHORIZED_USER_ERROR_MESSAGE = "You are not authorized to browse user information.";
-    public static final String SEARCH_ADMIN_MESSAGE_ERROR = "Only admins are able to review all users.";
     public static final String MASTER_ADMIN_MESSAGE_ERROR = "Master admin user can not be downgrade to user.";
     public static final String MASTER_ADMIN_MESSAGE_ERROR_BLOCK = "Master admin user can not be blocked.";
-    public static final String DELETE_TAG_MESSAGE_ERROR = "Only admin or creator can delete a tag.";
     public static final String DELETE_USER_MESSAGE_ERROR = "Only owner of this account can delete this profile.";
     public static final String MODIFY_USER_MESSAGE_ERROR = "Only user creator is able to update their profile information.";
-    public static final String UPDATE_USER_MESSAGE_ERROR = "Only user creator is able to update.";
     public static final String MODIFY_ADMIN_MESSAGE_ERROR = "Only admin is able to block or unblock individual users";
     public static final String UPDATE_PHONE_NUMBER_ERROR_MESSAGE = "Only owner admin can update phone number.";
     public static final String UPDATE_TO_ADMIN_ERROR_MESSAGE = "Only admin can make other user an admin";
     public static final String UPDATE_TO_USER_ERROR_MESSAGE = "Only admin can adjust the role of the user.";
-    public static final String MODIFY_POST_ERROR_MESSAGE = "Only admin or post creator can modify post.";
-    public static final String MODIFY_TAG_ERROR_MESSAGE = "Only tag creator can modify tag.";
-    public static final String USER_HAS_BEEN_BLOCKED_OR_DELETED = "User has been blocked or deleted.";
-    public static final String CREATE_TAG_MESSAGE_ERROR = "Only tag creator and admin can create a tag.";
     public static final String DELETE_PHONE_NUMBER_MESSAGE_ERROR = "Only owner admin can delete phone number.";
 
 
